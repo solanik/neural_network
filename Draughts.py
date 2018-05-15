6bb38f0960fd184ac452d5500155d75d6fe873f0
Filip <filwoz@gmail.com>
2018-05-15 19:27:39 +0200

    some changes

--- Draughts.py
+++ Draughts.py
@@ -3,15 +3,18 @@ import tensorflow as tf
 import math
 import numpy as np
 import os.path
+import logging
+
 
 class NeuralDraught:
     single_game_history = []
     training_history = {}
     game_number = 0
-    max_game_number = 1000
+    max_game_number = 2000
     training_number = 0
+    epsilon = 0.001
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    dir_path = os.path.join(dir_path, "model")
+    dir_path = os.path.join(dir_path, "model_" + str(max_game_number) + "_" + str(epsilon))
     model_directory = os.path.join(dir_path, "model.ckpt")
 
     def __init__(self, sess):
@@ -19,14 +22,13 @@ class NeuralDraught:
 
         with tf.name_scope('value_network'):
             self.input = tf.placeholder(
-                dtype=tf.float32, shape=(None,8*4*5), name='input')
+                dtype=tf.float32, shape=(None, 8*4*5), name='input')
             reshaped = tf.reshape(self.input, shape=[-1, 8, 4, 5], name="reshape")
             c1 = tf.layers.conv2d(reshaped, filters=32, kernel_size=3, strides=2, activation=tf.nn.relu, name="conv1")
-            flatten = tf.layers.flatten(c1,name="flatten")
-            h1 = tf.layers.dense(flatten, 512, activation=tf.nn.relu)
-            h2 = tf.layers.dense(h1, 1024, activation=tf.nn.relu)
-            h3 = tf.layers.dense(h2, 2048, activation=tf.nn.relu)
-            self.output = tf.layers.dense(h3, 1, activation=tf.nn.sigmoid)
+            flatten = tf.layers.flatten(c1, name="flatten")
+            h1 = tf.layers.dense(flatten, 256, activation=tf.nn.relu)
+            h2 = tf.layers.dense(h1, 128, activation=tf.nn.relu)
+            self.output = tf.layers.dense(h2, 1, activation=tf.nn.sigmoid)
             self.foutput = tf.squeeze(self.output, axis=1, name='flatten_output')
 
         self.saver = tf.train.Saver()
@@ -35,28 +37,36 @@ class NeuralDraught:
             self.expected = tf.placeholder(
                 dtype=tf.float32, shape=(None, 1), name='expected')
             cost = tf.reduce_mean((self.output - self.expected) ** 2)
-            self.train = tf.train.AdamOptimizer(epsilon=0.01).minimize(cost)
+            self.train = tf.train.AdamOptimizer(epsilon=NeuralDraught.epsilon).minimize(cost)
 
     def choose_board(self, possible_boards, player_moving):
         values = self.sess.run(self.foutput, feed_dict={
             self.input: possible_boards
         })
         sum_of_values = sum(values)
-        if (sum_of_values < 0.001):
+
+        if sum_of_values < 0.001:
             idx = np.random.choice(range(len(possible_boards)))
         else:
             weights = values/sum_of_values
-            #print("Wartosci ktore otrzymalismy: ", weights)
             idx = np.random.choice(range(len(possible_boards)), p=weights)
-        #print("Index ktory zwracamy: ", idx)
+
         board_str = ','.join(str(e) for e in possible_boards[idx])
         NeuralDraught.single_game_history.append({'PLAYER': player_moving, 'BOARD': board_str})
         return str(idx)
 
+#    def
+
+#    def consider_previous_choices(self, possible_boards, values):
+#        sum_of_values = sum(values)
+#        for i in range(len(values)):
+#            if  in
+
+
     def process_end_of_game(self, winner):
         NeuralDraught.game_number += 1
         self.update_current_history(winner)
-        print("Game:", NeuralDraught.game_number)
+        print("Game:", NeuralDraught.game_number, "Training:", NeuralDraught.training_number, "The winner is:", winner)
         if NeuralDraught.game_number == NeuralDraught.max_game_number:
             NeuralDraught.game_number = 0
             NeuralDraught.training_number += 1
@@ -79,19 +89,24 @@ class NeuralDraught:
         states = []
         values = []
         for key, value in NeuralDraught.training_history.items():
-            state = [int(s) for s in key.split(',')]
-            states.append(state)
-            values.append([value[0] / value[1]])
+            if value[1] >= (0.01 * NeuralDraught.max_game_number):
+                state = [int(s) for s in key.split(',')]
+                states.append(state)
+                values.append([value[0] / value[1]])
+                print(key, value)
 
-        self.sess.run(self.train, feed_dict={
-            self.input: states,
-            self.expected: values
-        })
+        for x in range(100):
+            self.sess.run(self.train, feed_dict={
+                self.input: states,
+                self.expected: values
+            })
 
         NeuralDraught.training_history = {}
 
+        #input("Press any key")
+
         save_path = self.saver.save(sess, NeuralDraught.model_directory)
-        print("Model saved in path: %s" % NeuralDraught.model_directory)
+        #print("Model saved in path: %s" % save_path)
 
 
 def parse_board(player_moving, possible_board_data):
@@ -105,14 +120,13 @@ def parse_board(player_moving, possible_board_data):
         what_piece = 0 if piece == "W,P" else \
                      1 if piece == "W,K" else \
                      2 if piece == "B,P" else \
-                     3 # piece == "B,K"
+                     3  # piece == "B,K"
         possible_board[row*20 + col*5 + what_piece] = 1
     player_moving_int = 1 if player_moving == "W" else \
-                        0 # player_moving == "B"
+                        0  # player_moving == "B"
     for i in range(32):
         possible_board[i*5 + 4] = player_moving_int
     return possible_board
-
 
 
 class MyTCPHandler(socketserver.StreamRequestHandler):
@@ -131,21 +145,18 @@ class MyTCPHandler(socketserver.StreamRequestHandler):
         data = self.rfile.readline().strip().decode("utf-8")
         if MyTCPHandler.operation == "":
             MyTCPHandler.operation = data
-            #print("Received,", MyTCPHandler.operation)
             self.request.sendall(b'0')
         elif MyTCPHandler.operation == "GET_MOVE":
             MyTCPHandler.operation = ""
-            #print("Parsing boards,", data)
             player_moving = data[0]
             possible_boards_with_moves = data[4:-3].split("CM, CM")
             possible_boards = []
             for possible_board_with_moves in possible_boards_with_moves:
                 possible_board_data = possible_board_with_moves.split("M, B")[1][1:-2]
                 possible_board = parse_board(player_moving, possible_board_data)
-                #print("Possible board: ", possible_board)
                 possible_boards.append(possible_board)
-            agentResult = agent.choose_board(possible_boards, player_moving)
-            self.request.sendall(agentResult.encode())
+            agent_result = agent.choose_board(possible_boards, player_moving)
+            self.request.sendall(agent_result.encode())
         elif MyTCPHandler.operation == "END_GAME":
             MyTCPHandler.operation = ""
             winner_is = "X"
@@ -159,27 +170,32 @@ class MyTCPHandler(socketserver.StreamRequestHandler):
                 winner_is = "W" if white_value > black_value else \
                             "B" if black_value > white_value else \
                             "X"  # value == value
-                #print("Game ended with result: ", winner_is)
-                #input("Press Enter to continue...")
-            #print("Game ended with result: ", winner_is)
+            #print("Winner is:", winner_is)
             agent.process_end_of_game(winner_is)
             self.request.sendall("READY".encode())
-        #print("END OF HANDLE")
+
+
+def is_folder_empty(folder):
+    return [f for f in os.listdir(folder) if not f.startswith('.')] == []
+
 
 if __name__ == "__main__":
     HOST, PORT = "192.168.0.103", 8080
 
-    if not os.path.isdir(NeuralDraught.dir_path):
+    logging.getLogger().setLevel(logging.INFO)
+
+    model_exists = os.path.isdir(NeuralDraught.dir_path)
+    if not model_exists:
         os.makedirs(NeuralDraught.dir_path)
 
     with tf.Session() as sess:
         agent = NeuralDraught(sess)
 
-        if os.path.isfile(NeuralDraught.model_directory):
+        sess.run(tf.global_variables_initializer())
+        print("Model initialized.")
+        if not is_folder_empty(NeuralDraught.dir_path):
             agent.saver.restore(sess, NeuralDraught.model_directory)
             print("Model restored.")
-        else:
-            sess.run(tf.global_variables_initializer())
 
         # Create the server, binding to localhost on port 9999
         server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
